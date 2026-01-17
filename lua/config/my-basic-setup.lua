11a3bb6d3b4249961dcf17304f5bb5170ff637da
Banacount <uniquejmal@gmail.com>
2026-01-17 20:52:23 +0800

    Anotha update

--- lua/config/my-basic-setup.lua
+++ lua/config/my-basic-setup.lua
@@ -10,3 +10,20 @@ vim.keymap.set("v", "<leader>y", function() copyToClipboard() end, { desc = "Cop
 
 -- General stuffs
 vim.keymap.set("n", "<leader>f", ':Oil<CR>', { desc = "Open oil.nvim using leader" })
+
+-- Debugging stuffs
+-- Diagnose command for syntax errors
+vim.api.nvim_create_user_command('Diagnose', function(opts)
+    if opts.fargs[1] == "list" then
+        vim.diagnostic.setqflist()
+        print("Opening diagnostic list!")
+    else
+        vim.diagnostic.open_float()
+        print("Diagnostic float opened!")
+    end
+end, {
+    nargs = '*',
+    complete = function()
+        return { "list" }
+    end
+})
